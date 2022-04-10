f4341f7c44dfcc9bfbcdbdd15232b2429cffa138
quy nang <quynang2014@gmail.com>
2022-04-11 03:11:27 +0700

    Update params

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -6,8 +6,8 @@ class TwitterAccount < ApplicationRecord
 
   def client
     Twitter::REST::Client.new do |config|
-      config.consumer_key        = Rails.application.credentials.dig(:twitter, :api_key)
-      config.consumer_secret     = Rails.application.credentials.dig(:twitter, :api_secret)
+      config.consumer_key        = ENV['TWITTER_KEY']
+      config.consumer_secret     = ENV['TWITTER_SECRET']
       config.access_token        = token
       config.access_token_secret = secret
     end
